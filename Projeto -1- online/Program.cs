032fb73b6eafc0dccdb15a29bac8ed7345263bf5
cleasholdorf <you@example.com>
2022-04-04 12:38:01 -0300

    primeiro

---
+++ Projeto -1- online/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projeto__1__online
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
